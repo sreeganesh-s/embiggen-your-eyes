dd38740f6632f7404da2080a15df9ed9f160c880
sreeganesh-s <sreeganeshsreejith@yahoo.com>
2025-10-05 05:34:43 +0530

    earth submitted

--- ganesh/Earth/earth-data.js
+++ ganesh/Earth/earth-data.js
@@ -7,13 +7,13 @@ export const earthData = {
     atmosphere: 'Nitrogen (78%), Oxygen (21%), trace gases'
   },
   images: [
-    { url: 'https://eoimages.gsfc.nasa.gov/images/imagerecords/57000/57730/earth_lrg.jpg', title: 'Blue Marble (NASA)'} ,
+    { url: 'https://t3.ftcdn.net/jpg/01/94/08/00/360_F_194080021_Qqc5gJLp4uNQpSStlbPdbQdeYUXt7PHN.jpg', title: 'Earth (stock)'} ,
     { url: 'https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg', title: 'Earth from Apollo 17 (NASA)'} ,
     { url: 'https://earthobservatory.nasa.gov/ContentFeature/BlueMarble/Images/land_shallow_topo_4096.jpg', title: 'Topography (NASA)'}
   ],
   features: [
-    { name: 'Mount Everest', type: 'Mountain', description: 'Highest point above sea level on Earth.', image: 'https://upload.wikimedia.org/wikipedia/commons/1/12/Mount_Everest_from_Gokyo_Ri_Nov2008.jpg', credit: 'Wikimedia Commons' },
-    { name: 'Mariana Trench', type: 'Ocean trench', description: 'Deepest part of the world\'s oceans.', image: 'https://upload.wikimedia.org/wikipedia/commons/6/65/Mariana_trench_cross_section.jpg', credit: 'NOAA' }
+    { name: 'Mount Everest', type: 'Mountain', description: 'Highest point above sea level on Earth.', image: 'https://cdn.britannica.com/17/83817-050-67C814CD/Mount-Everest.jpg', credit: 'Encyclopaedia Britannica' },
+    { name: 'Mariana Trench', type: 'Ocean trench', description: 'Deepest part of the world\'s oceans.', image: 'https://media.istockphoto.com/id/1152368018/photo/mariana-trench-the-deepest-depths.jpg?s=612x612&w=0&k=20&c=KyREn8VLnLvxC2HfOAb4-QfZ7zDD33kGsqRk7FYauyo=', credit: 'iStockphoto' }
   ],
   missions: [
     { name: 'Landsat program', year: '1972–present', summary: 'Ongoing Earth-observing satellites providing high-quality imagery for science and resource management.', credit: 'NASA/USGS' },
